2251c3a7075785116888246699cb02624d88f236
Martin Flasar <christian.flasar@gmail.com>
2024-01-11 05:51:46 +0100

    Initial Commit with ver1 of view switching

---
+++ Views/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Book_Worm.Views
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void Sign_In(object sender, RoutedEventArgs e)
+        {
+            Trace.WriteLine("Sign in!");
+        }
+    }
+}
